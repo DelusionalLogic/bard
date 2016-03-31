d7b7c2bd08f46a11766a98ee85dfc9ea7e651a55
Jesper Jensen <delusionalLogic@gmail.com>
2016-03-31 14:53:48 +0200

    Better parser

--- src/formatter.c
+++ src/formatter.c
@@ -21,91 +21,126 @@
 #include "myerror.h"
 #include "logger.h"
 
+struct Parser {
+	char* str;
+	char cur;
+	size_t index;
+	size_t len;
+};
+
+void parser_start(struct Parser* parser, char* str) {
+	parser->str = str;
+	parser->index = 0;
+	parser->len = strlen(str);
+	parser->cur = parser->len == 0 ? '\0' : parser->str[parser->index];
+}
+
+bool parser_isDone(struct Parser* parser) {
+	return parser->index > parser->len;
+}
+
+void parser_eat(struct Parser* parser) {
+	if(parser_isDone(parser))
+		return;
+	parser->index++;
+
+	parser->cur = parser_isDone(parser) ? '\0' : parser->str[parser->index];
+}
+
+char* parseArrayData(jmp_buf jmpBuf, struct Parser* parser, const struct FormatArray *arrays[], const size_t arraysCnt);
+
 bool formatter_format(jmp_buf jmpBuf, char* input, const struct FormatArray *arrays[], size_t arraysCnt, char** Poutput) {
 	Vector output;
 	vector_init(jmpBuf, &output, sizeof(char), 512);
-	size_t inLen = strlen(input);
+
+	struct Parser parser;
+	struct Parser* p = &parser;
+	parser_start(p, input);
+
 	size_t start = 0;
-	for(size_t i = 0; i < inLen; i++) {
-		if(input[i] == '\\') {
-			if(input[i+1] == '\\') {
-				vector_putListBack(jmpBuf, &output, input + start, (i-start));
+	while(!parser_isDone(p)) {
+		if(p->cur == '\\') {
+			parser_eat(p);
+			if(p->cur == '\\') {
 				vector_putListBack(jmpBuf, &output, "\\", 1);
-				i++;
-				start = i+1;
-			}else if(input[i+1] == '$') {
-				vector_putListBack(jmpBuf, &output, input + start, (i-start));
+				parser_eat(p);
+				start = p->index+1;
+			}else if(p->cur == '$') {
 				vector_putListBack(jmpBuf, &output, "$", 1);
-				i++;
-				start = i+1;
+				parser_eat(p);
+				start = p->index+1;
 			}else{
-				log_write(LEVEL_ERROR, "No escape code \\%c", input[i]);
+				log_write(LEVEL_ERROR, "No escape code \\%c", p->cur);
 				return false;
 			}
-		}else if(input[i] == '$') {
-			vector_putListBack(jmpBuf, &output, input + start, i-start);
-
-			//Find the array
-			const struct FormatArray* formatArr = NULL;
-			{
-				Vector ident;
-				vector_init(jmpBuf, &ident, sizeof(char), 12);
-
-				i++;
-				while(input[i] != '[') {
-					vector_putBack(jmpBuf, &ident, input + i);
-					i++;
-				}
-				vector_putListBack(jmpBuf, &ident, "\0", 1);
-
-				for(size_t i = 0; i < arraysCnt; i++) {
-					if(strcmp(arrays[i]->name, ident.data) == 0) {
-						formatArr = arrays[i];
-						break;
-					}
-				}
-
-				if(formatArr == NULL) {
-					log_write(LEVEL_ERROR, "No array named %s", ident.data);
-					vector_kill(&ident);
-					longjmp(jmpBuf, MYERR_USERINPUTERR);
-				}
-
-				vector_kill(&ident);
-			}
-
-			//Find the value
-			char* value;
-			{
-				Vector key;
-				vector_init(jmpBuf, &key, sizeof(char), 12);
-
-				i++;
-				while(input[i] != ']') {
-					vector_putBack(jmpBuf, &key, input + i);
-					i++;
-				}
-				vector_putListBack(jmpBuf, &key, "\0", 1);
-
-				PWord_t pval;
-				JSLG(pval, formatArr->array, key.data);
-
-				if(pval == NULL) {
-					log_write(LEVEL_ERROR, "No key named \"%s\" in \"%s\"", key.data, formatArr->name);
-					vector_kill(&key);
-					longjmp(jmpBuf, MYERR_USERINPUTERR);
-				}
-
-				value = *pval;
-
-				vector_kill(&key);
-			}
-
+		}else if(p->cur == '$') {
+			parser_eat(p);
+			char* value = parseArrayData(jmpBuf, p, arrays, arraysCnt);
 			vector_putListBack(jmpBuf, &output, value, strlen(value));
-			start = i+1;
+		}else {
+			vector_putBack(jmpBuf, &output, &p->cur);
 		}
+		parser_eat(p);
 	}
-	vector_putListBack(jmpBuf, &output, input + start, (inLen - start)+1);
+	vector_putListBack(jmpBuf, &output, input + start, (p->len - start)+1);
 	*Poutput = vector_detach(&output);
 	return true;
+}
+
+char* parseArrayData(jmp_buf jmpBuf, struct Parser * p, const struct FormatArray *arrays[], const size_t arraysCnt) {
+	//Find the array
+	const struct FormatArray* formatArr = NULL;
+	{
+		Vector ident;
+		vector_init(jmpBuf, &ident, sizeof(char), 12);
+
+		while(p->cur != '[' && !parser_isDone(p)) {
+			vector_putBack(jmpBuf, &ident, &p->cur);
+			parser_eat(p);
+		}
+		vector_putListBack(jmpBuf, &ident, "\0", 1);
+
+		for(size_t i = 0; i < arraysCnt; i++) {
+			if(strcmp(arrays[i]->name, ident.data) == 0) {
+				formatArr = arrays[i];
+				break;
+			}
+		}
+
+		if(formatArr == NULL) {
+			log_write(LEVEL_ERROR, "No array named %s", ident.data);
+			vector_kill(&ident);
+			longjmp(jmpBuf, MYERR_USERINPUTERR);
+		}
+
+		vector_kill(&ident);
+	}
+
+	//Find the value
+	char* value;
+	{
+		Vector key;
+		vector_init(jmpBuf, &key, sizeof(char), 12);
+
+		parser_eat(p);
+		while(p->cur != ']' && !parser_isDone(p)) {
+			vector_putBack(jmpBuf, &key, &p->cur);
+			parser_eat(p);
+		}
+		vector_putListBack(jmpBuf, &key, "\0", 1);
+
+		PWord_t pval;
+		JSLG(pval, formatArr->array, key.data);
+
+		if(pval == NULL) {
+			log_write(LEVEL_ERROR, "No key named \"%s\" in \"%s\"", key.data, formatArr->name);
+			vector_kill(&key);
+			longjmp(jmpBuf, MYERR_USERINPUTERR);
+		}
+
+		value = *pval;
+
+		vector_kill(&key);
+	}
+	return value;
 }
